f7792802484aeaa5a6a8afefadab0f4c8cc4ef53
boonboks <XXXlinkoln@yandex.ru>
2021-11-20 12:12:19 +0300

    Пагинация

--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Models\WebPage\Product; 
-use App\Models\WebPage\Category; 
-use App\Http\Resources\WebPage\ProductCollection; 
-use App\Http\Resources\WebPage\CategoryCollection; 
+use App\Models\WebPage\Product;
+use App\Models\WebPage\Category;
+use App\Http\Resources\WebPage\ProductCollection;
+use App\Http\Resources\WebPage\CategoryCollection;
+use App\Http\Resources\WebPage\ProductResource;
+use Illuminate\Support\Collection;
+use Illuminate\Http\JsonResponse;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -25,21 +28,23 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::get('/products', function (Request $request) {
     $products = Product::paginate();
-    $product_api = new ProductCollection($products); 
-    return $product_api->toJson();  
-}); 
+    $product_api = new ProductCollection($products);
+    return $product_api->toJson();
+});
 
 
 Route::get('/category', function (Request $request) {
     $category = Category::where('category_id', null)->get();
-    $category_api = new CategoryCollection($category); 
-    return $category_api->toJson();  
-}); 
+    $category_api = new CategoryCollection($category);
+    return $category_api->toJson();
+});
 
 
 Route::get('/category/{id}', function (Request $request, $id) {
-    $products = Category::find($id)->products; 
-    $products_api = new ProductCollection($products); 
-    return $products_api;  
-}); 
+    $products = Category::find($id)->products()->paginate(5);
+    $product_api = new ProductCollection($products);
+    return $product_api;
 
+    // $data =  Category::find($id)->products()->paginate(5);
+    // return ProductResource::collection($data)->response()->getData(true);
+});
